edee47fc5f50e793ef393f0dd44cb582c9e2c44f
Pétterson Castro <pettersoncastro21@gmail.com>
2026-08-11 20:31:21 -0300

    Create exercicio_variavel.py

---
+++ semana01/exercicio_variavel.py
@@ -0,0 +1,19 @@
+IDADE = 21
+NOME = "Péterson"
+CPF = "123.456.789.09"
+PESO  = 70
+DATADENASC ="10/12/86"
+CIDADE = "Mafra"
+ESTADO  = "Santa Catarina"
+VELOCIDADE = 180
+PRECO = 19,99
+
+
+print("Sua idade:" , IDADE , "anos")
+print("Seu nome:" , NOME)
+print("Seu CPF:" , CPF)
+print("Nascimento:" , DATADENASC)
+print("Sua cidade:" , CIDADE)
+print("Seu estado:" ,  ESTADO)
+print("Sua velociddade:" ,VELOCIDADE , "km/h")
+print("preco:",PRECO)
