1419329d5a7933295cbb8b95ae4f044edaaec0b7
Pétterson Castro <pettersoncastro21@gmail.com>
2026-08-24 19:47:36 -0300

    Add age classification algorithm in Python

---
+++ semana03/exercício_4.py
@@ -0,0 +1,11 @@
+'''Escreva um algoritmo em Python que dada a idade de uma pessoa,
+determine sua classificação:
+maior de idade;
+menor de idade;'''
+
+idade = int(input("digite a idade:"))
+if idade >=18:
+    print("maior de idade:")
+else:
+    print("menor de idade:")
+    
